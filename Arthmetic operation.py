5e75b97ba810bbc86d11dcfae4520b4dad1e6f93
sagar12345678214 <81381175+sagar12345678214@users.noreply.github.com>
2021-03-25 23:00:50 +0530

    Add files via upload

---
+++ Arthmetic operation.py
@@ -0,0 +1,16 @@
+print('Arthmetic Operation')
+
+x=5
+y=4
+
+print('Addition:')
+print(x+y)
+
+Print('Substraction:')
+print(x-y)
+
+Print('Multiplication:')
+print(x*y)
+
+Print('Division:')
+print(x/y)
